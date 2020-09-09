ff2cc6c7cc5530db4340ea2c6e3a78b350e21ffe
venkat p <venkat@vectramind.com>
2020-09-09 10:10:14 +0530

    Venkat(Navya)
    Client logo updated

--- src/pages/kiosk/theme1/components/themeonetopbar.tsx
+++ src/pages/kiosk/theme1/components/themeonetopbar.tsx
@@ -6,7 +6,7 @@ import { interval } from "rxjs";
 import { ParentContext } from '../../Container/kioskcontext';
 import { getCurrentDateTimeRequest } from '../../../../store/actions';
 import { getDateTimeInterval } from '../../../../helpers/helpersIndex';
-
+import clientLogo from '../../../../images/aldaraLogo.png';
 let subscribe;
 
 const ThemeOneTopBar: React.FC = () => {
@@ -39,11 +39,14 @@ const ThemeOneTopBar: React.FC = () => {
 
             <div className="topbar">
                 <Row className="m-0 topheader">
-                    <Col sm="6" xs="6">
-                        <img src={headerlogo} alt="" width="150" />
+                    <Col sm="4" xs="4">
+                        <img src={headerlogo} alt="" width="160" />
                     </Col>
-                    <Col sm="6" xs="6" className="date-time">
+                    <Col sm="4" xs="4" className="date-time">
                         {dataTime && <context.ThemeOneTime />}
+                    </Col>
+                    <Col sm="4" xs="4" className="text-right">
+                        <img src={clientLogo} alt="" width="180"/>
                     </Col>
                 </Row>
             </div>
