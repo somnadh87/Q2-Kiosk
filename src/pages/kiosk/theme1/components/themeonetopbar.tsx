1ac3666282644b005e0e4d885cfe7dc03ede9f0d
venkat p <venkat@vectramind.com>
2020-11-20 15:56:21 +0530

    Venkat(Navya)
    Client logo changed to Aldara

--- src/pages/kiosk/theme1/components/themeonetopbar.tsx
+++ src/pages/kiosk/theme1/components/themeonetopbar.tsx
@@ -6,7 +6,8 @@ import { interval } from "rxjs";
 import { ParentContext } from '../../Container/kioskcontext';
 import { getCurrentDateTimeRequest } from '../../../../store/actions';
 import { getDateTimeInterval } from '../../../../helpers/helpersIndex';
-import clientLogo from '../../../../images/vmcare-light.svg';
+import clientLogo from '../../../../images/aldaraLogo.png';
+// import clientLogo from '../../../../images/vmcare-light.svg';
 let subscribe;
 
 const ThemeOneTopBar: React.FC = () => {
